a8a4eb772bf09e59e4335d689e6251807446a3e4
Simon <simon.hellbeck@skola.taby.se>
2023-05-19 23:56:57 +0200

    Snälla ge mig bra betyg :)))

---
+++ netflix.js
@@ -0,0 +1,11 @@
+
+sökbar.addEventListener("keydown", (e) => {
+    if (e.key == Enter && searchbar != "")
+    let sökning = document.getElementById("sökbar")
+    console.log(sökning)
+}
+
+
+
+
+
